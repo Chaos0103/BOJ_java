4993d1cfdc146598ac6c44f80ec025c44aecbbc2
chaos0103 <lyt1228@naver.com>
2022-12-18 17:51:03 +0900

    카드2

---
+++ src/s4/P2164.java
@@ -0,0 +1,25 @@
+package s4;
+
+import java.util.*;
+
+public class P2164 {
+
+    private static final Queue<Integer> q = new LinkedList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);
+        }
+
+        while (q.size() > 1) {
+            q.poll();
+            q.offer(q.poll());
+        }
+
+        System.out.println(q.peek());
+    }
+}
